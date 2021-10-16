12485e5d836cb87e39c1c3be49bf49a5c97f88a8
Pavel <pavel.ahmed.pavel.ahmed@gmail.com>
2021-10-16 13:27:48 +0600

    Namespace fix for the api route and api controllers directory

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
+Route::group(['namespace' => "\App\Http\Controllers\Api", 'as' => 'api.'], function () {
 
     Route::post('login', 'LoginController@login')->name('login');
 
