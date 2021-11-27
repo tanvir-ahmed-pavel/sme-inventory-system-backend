868dc2c298f6a98b19dc455cd652c56c4861b8b1
Pavel <pavel.ahmed.pavel.ahmed@gmail.com>
2021-11-28 00:27:34 +0600

    Namespace and token generation error fixed!

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => "\App\Http\Controllers\Api", 'as' => 'api.'], function () {
 
     Route::post('login', 'LoginController@login')->name('login');
+    Route::get('/', function (){
+        return response()->json("token cleared successfully", 200);
+    });
 
     Route::post('register', 'RegisterController@register')->name('register');
 
